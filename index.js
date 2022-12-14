24ee65bfe4e51b221ea183c0e00346d3bcf06dfa
Alex <azuniga1@my.centennialcollege.ca>
2022-12-14 01:46:30 -0500

    udated UI and created a page for db connection

--- index.js
+++ index.js
@@ -1,55 +1,35 @@
-// const oracledb = require('oracledb');
-
-
-// async function run() {
-
-//   let connection;
-
-//   try {
-
-//     connection = await oracledb.getConnection({ user: "demonode", password: "XXXX", connectionString: "localhost/xepdb1" });
-
-//     console.log("Successfully connected to Oracle Database");
-
-//   } catch (err) {
-//     console.error(err);
-//   } finally {
-//     if (connection) {
-//       try {
-//         await connection.close();
-//       } catch (err) {
-//         console.error(err);
-//       }
-//     }
+//express backend
+// const data = [
+//   {"id": 123, "Name": "Columbia", "Options": "1 lb. Whole Bean", "Description": "Columbian Beans","Image": "url", "Status":"In stock", "Qty": 1, "Price": 10.42, "Total": 10.42},
+//   {
+//       "id": 125, "Name": "Ethiopia", "Options": "1 lb. Whole Bean", "Description": "Ethiopian Beans", "Image": "url", "Status":"Out of stock", "Qty": 1, "Price": 10.00, "Total": 10.00
 //   }
-// }
+// ];
+// const data2 = [ {"IDBASKET": 3, "DTSTAGE": "", "NOTES": "Customer called to confirm shipment", "SHIPPER": "UPS","SHIPPINGNUM":"X2S5LL902G"}]
+// var express = require('express');
+// var app = express();
 
-// run();
-const data = [
-  {"id": 123, "Name": "Columbia", "Options": "1 lb. Whole Bean", "Description": "Columbian Beans","Image": "url", "Status":"In stock", "Qty": 1, "Price": 10.42, "Total": 10.42},
-  {
-      "id": 125, "Name": "Ethiopia", "Options": "1 lb. Whole Bean", "Description": "Ethiopian Beans", "Image": "url", "Status":"Out of stock", "Qty": 1, "Price": 10.00, "Total": 10.00
-  }
-];
-var express = require('express');
-var app = express();
+// // set the view engine to ejs
+// app.set('view engine', 'ejs');
+// app.use(express.static("public"));
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use(express.static("public"));
-
-app.get('/', function(req, res) {
-  res.render('home', {title: 'Home', items: data});
-});
-app.get('/editproduct/:id', function(req, res) {
-  res.render('editProduct', {title: 'Edit Product', items: data[0]});
-});
-app.get('/createproduct', function(req, res) {
-  res.render('createProduct', {title: 'Create Product', items: data});
-});
-app.get('/checkout', function(req, res) {
-  res.render('checkout', {title: 'Checkout', items: data});
-});
-
-app.listen(3000);
-console.log('Server is listening on port 3000');
+// app.get('/', function(req, res) {
+//   res.render('productList', {title: 'Product List', items: data});
+// });
+// app.get('/editproduct', function(req, res) {
+//   res.render('editProduct', {title: 'Edit Product', items: data[0]});
+// });
+// app.get('/createproduct', function(req, res) {
+//   res.render('createProduct', {title: 'Create Product', items: data});
+// });
+// app.get('/basket', function(req, res) {
+//   res.render('basket', {title: 'basket', items: data});
+// });
+// app.get('/updatestatus', function(req, res) {
+//   res.render('updateStatus', {title: 'Update Status', items: data2});
+// });
+// app.get('/reports', function(req, res) {
+//   res.render('reports', {title: 'Reports'});
+// });
+// app.listen(3000);
+// console.log('Server is listening on port 3000');
